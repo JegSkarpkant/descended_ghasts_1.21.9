1090ebc3bf267833b7f922a901e8936ebc566e1f
jegskarpkant <howard.orskaug@gmail.com>
2025-12-16 18:40:25 +0100

    Made the entire mod lmao, might need some tweaks tho

---
+++ src/main/java/descendedghasts/DescendedGhasts.java
@@ -0,0 +1,16 @@
+package descendedghasts;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DescendedGhasts implements ModInitializer {
+	public static final String MOD_ID = "descended-ghasts";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/descendedghasts/DescendedGhastsClient.java
@@ -0,0 +1,51 @@
+package descendedghasts;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.GhastEntity;
+import org.lwjgl.glfw.GLFW;
+
+
+public class DescendedGhastsClient implements ClientModInitializer {
+
+    public static KeyBinding Descend_Key;
+    private static final KeyBinding.Category Descend_Category = new KeyBinding.Category(
+            Identifier.of("category.descendedghasts.controls")
+    );
+    @Override
+    public void onInitializeClient() {
+        System.out.println("It's working! It's working!");
+
+        Descend_Key = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.descendedghasts.descend",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_LEFT_CONTROL,
+                Descend_Category
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (Descend_Key.isPressed()) {
+                if (client.player != null) {
+                    if (client.player.hasVehicle()) {
+                        Entity vehicle = client.player.getVehicle();
+                        if (vehicle instanceof GhastEntity) {
+                            double descendSpeed = -0.2;
+                            vehicle.setVelocity(
+                                    vehicle.getVelocity().getX(),
+                                    descendSpeed,
+                                    vehicle.getVelocity().getZ()
+                            );
+                        }
+                    }
+                }
+            }
+        });
+
+    }
+}
